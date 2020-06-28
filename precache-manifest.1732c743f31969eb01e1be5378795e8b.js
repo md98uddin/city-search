ad1cc475e38c94afa2a17c0b7f63d375ee0acc8c
md uddin <mduddin1998@yahoo.com>
2020-06-28 04:38:37 -0400

    Updates

---
+++ precache-manifest.1732c743f31969eb01e1be5378795e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa65bb0c7ba5a7df558c94532be63383",
+    "url": "/city-search/index.html"
+  },
+  {
+    "revision": "28c6d2217ff739741b7e",
+    "url": "/city-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9fdb57d66ef7a795181f",
+    "url": "/city-search/static/js/2.c4950073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/city-search/static/js/2.c4950073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c6d2217ff739741b7e",
+    "url": "/city-search/static/js/main.b9b07307.chunk.js"
+  },
+  {
+    "revision": "5e48ab0f8a9959aad62f",
+    "url": "/city-search/static/js/runtime-main.9716a5a0.js"
+  }
+]);
